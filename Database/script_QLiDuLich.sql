f702c4b9520340ced76ffc416b4bdc417cd72502
Lumine11 <kt061103@gmail.com>
2023-05-23 00:03:11 +0700

    Scrpit tạo database
    Upload script tạo các database dùng trong quá trình học

---
+++ Database/script_QLiDuLich.sql
@@ -0,0 +1,101 @@
+﻿-- MSSV: 21120580
+-- Họ tên: Trần Thị Kim Trinh
+
+
+create database QLiDuLich
+go
+use QLiDuLich
+go
+
+create table Tinh_TP(
+QuocGia char(10),
+MaTinhThanh char(10),
+TenTT char(10),
+SoDan int, check (SoDan > 0),
+DienTich float, check (DienTich > 0), -- đề ghi int nhưng trong bảng lại biểu diễn số thực nên em đổi lại
+DiemDLUaThichNhat char(10),
+constraint PK_Tinh_TP primary key (QuocGia, MaTinhThanh)
+)
+
+create table QUOCGIA(
+MaQG char(10),
+TenQG nchar(10) unique,
+ThuDo char(10),
+constraint PK_QuocGia primary key (MaQG)
+)
+create table DIEM_DL(
+MaDiaDiem char(10),
+TenDiaDiem nchar(50),
+TinhTP char(10),
+QuocGia char(10),
+DacDiem nchar(100)
+constraint PK_DiemDL primary key (MaDiaDiem,TinhTP,QuocGia)
+)
+
+alter table Tinh_TP
+add constraint FK_Tinh_QG
+foreign key (QuocGia)
+references QUOCGIA(MaQG)
+
+alter table QUOCGIA
+add constraint FK_QG_T
+foreign key (MaQG, ThuDo)
+references Tinh_TP(QuocGia,MaTinhThanh)
+
+alter table DIEM_DL
+add constraint FK_Diem_TinhTP
+foreign key (QuocGia, TinhTP)
+references Tinh_TP(QuocGia, MaTinhThanh)
+
+alter table Tinh_TP
+add constraint FK_Tinh_DiemDL
+foreign key (DiemDLUaThichNhat, MaTinhThanh, QuocGia)
+references DIEM_DL(MaDiaDiem, TinhTP, QuocGia)
+
+-- do nhập theo thứ tự đề bài thì sẽ lỗi nên em đổi lại nhập quốc gia trước
+insert into QUOCGIA
+values('QG001' , N'Việt Nam', null)
+insert into QUOCGIA
+values('QG002' , N'Nhật Bản', null)
+
+insert into Tinh_TP
+values ('QG001', 'TT001', N'Hà Nội', 2500000 ,927.39, null)
+insert into Tinh_TP
+values ('QG001', 'TT002', N'Huế', 5344000 ,5009.00, null)
+insert into Tinh_TP
+values ('QG002', 'TT001', N'Tokyo', 12084000 ,2187.00, null)
+insert into Tinh_TP
+values ('QG002', 'TT002', N'Osaka', 18000000 ,221.96, null)
+
+update QUOCGIA
+set ThuDo = 'TT001'
+where MAQG = 'QG001'
+
+update QUOCGIA
+set ThuDo = 'TT001'
+where MAQG = 'QG002'
+
+insert into DIEM_DL 
+values('DD001', N'Văn Miếu', 'TT001', 'QG001', N'Di tích cổ')
+insert into DIEM_DL 
+values('DD002', N'Hoàng lăng', 'TT002', 'QG001', N'Di tích cổ')
+insert into DIEM_DL 
+values('DD001', N'Núi Fuji', 'TT001', 'QG002', N'Núi lửa ngưng hoạt động cao nhất Nhật Bản')
+insert into DIEM_DL 
+values('DD001', N'Minami', 'TT002', 'QG002', N'quê hương của cây cầu Shinsaibashi')
+insert into DIEM_DL 
+values('DD002', N'Lâu đài Osaka', 'TT002', 'QG002', N'Chứa bảo tàng thông tin lịch sử của lâu đài và Toyotomi Hideyoshi')
+
+
+update Tinh_TP
+set DiemDLUaThichNhat= 'DD001'
+where QuocGia = 'QG001' and MaTinhThanh = 'TT001' 
+
+update Tinh_TP
+set DiemDLUaThichNhat= 'DD002'
+where QuocGia = 'QG001' and MaTinhThanh = 'TT002' 
+
+update Tinh_TP
+set DiemDLUaThichNhat= 'DD001'
+where QuocGia = 'QG002' and MaTinhThanh = 'TT002' 
+
---
+++ Database/script_QuanLiLopHoc.sql
@@ -0,0 +1,125 @@
+﻿create database QLiLopHoc21120580
+go
+use QLiLopHoc21120580
+go
+
+create table LopHoc(
+MaLop char(5), 
+TenLop nvarchar(40) unique, 
+LopTruong char(4), 
+SiSo int default 0, 
+Phong char(3)
+constraint PK_Lop primary key (MaLop)
+)
+create table ToHT(
+IDLop char(5), 
+STT int,
+TenTo nvarchar(20),
+ToTruong char(4)
+constraint PK_ToHT primary key (IDLop, STT)
+)
+create table HocVien(
+MaHV char(4),
+HoTen nvarchar(30),
+NgaySinh date,
+Phai nchar(5) check (Phai in (N'nam', N'nữ')),
+IDLop char(5),
+IDTo int,
+constraint PK_HV primary key (MaHV)
+)
+
+alter table LopHoc
+add constraint FK_LH_HV
+foreign key (LopTruong)
+references HocVien(MaHV)
+
+alter table ToHT
+add constraint FK_To_Lop
+foreign key (IDLop)
+references LopHoc(Malop)
+
+alter table ToHT
+add constraint FK_To_HV
+foreign key (ToTruong)
+references HocVien(MaHV)
+
+alter table HocVien
+add constraint FK_HV_To
+foreign key (IDLop, IDTo)
+references ToHT(IDLop, STT)
+
+---- Nhập liệu
+insert into LopHoc
+values('LH011', N'Lớp một 1', null, 2, 'P01')
+
+insert into LopHoc
+values('LH012', N'Lớp một 2', null, 2, 'P02')
+
+insert into LopHoc
+values('LH013', N'Lớp một 3', null,0 , 'P03')
+
+insert into ToHT
+values ('LH011',1 ,N'Tổ 1', null)
+
+insert into ToHT
+values ('LH012',1 ,N'Tổ 1', null)
+
+insert into ToHT
+values ('LH012',2 ,N'Tổ 2', null)
+
+insert into HocVien
+values ('HV01',N'Bảo Duy','2015-01-12','nam','LH012',1)
+
+insert into HocVien
+values ('HV02',N'Kiều Linh','2015-04-05',N'nữ','LH012',1)
+
+insert into HocVien
+values ('HV06',N'Mai Anh','2015-01-01',N'nữ','LH012',2)
+
+insert into HocVien
+values ('HV03',N'Trúc An','2015-07-15',N'nữ','LH011',1)
+
+insert into HocVien
+values ('HV04',N'Gia Hào','2015-09-28','nam','LH012',2)
+
+update LopHoc
+set LopTruong = 'HV03'
+where MaLop = 'LH011'
+
+update LopHoc
+set LopTruong = 'HV01'
+where MaLop = 'LH012'
+
+update ToHT
+set ToTruong = 'HV04'
+where IDLop = 'LH011' and STT = 1
+
+update ToHT
+set ToTruong = 'HV02'
+where IDLop = 'LH012' and STT = 1
+
+update ToHT
+set ToTruong = 'HV06'
+where IDLop = 'LH012' and STT = 2
+
+-- truy vấn
+--Cho danh sách các lớp chưa có học viên
+select MaLop from LopHoc
+except
+select IDLop from HocVien
+
+--Cho danh sách các học viên (mã hv, tên hv, tên tổ, tên tổ trưởng) của tổ 1 trong
+--lớp một 2
+select HV.MaHV, HV.HoTen, T.TenTo, HV2.HoTen
+from LopHoc LH join ToHT T on LH.MaLop = T.IDLop
+		join HocVien HV on T.IDLop = HV.IDLop AND T.STT = HV.IDTo
+		join HocVien HV2 on T.ToTruong = HV2.MaHV
+where STT = 1 AND TenLop = N'Lớp một 2'
+
+-- Đếm số học viên trong mỗi tổ của lớp một 2.
+select STT, count (distinct MAHV)
+from LopHoc LH join ToHT T on LH.MaLop = T.IDLop
+		join HocVien HV on T.IDLop = HV.IDLop AND T.STT = HV.IDTo
+where TenLop = N'Lớp một 2'
+group by STT
+
